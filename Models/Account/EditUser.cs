49e2e892823efd8c47d60fc1a601ce6f8a60fe89
MuhammadAhmed01 <muhammadahme2019@fau.edu>
2024-03-05 18:50:54 -0500

    Initial commit

--- Models/Account/EditUser.cs
+++ Models/Account/EditUser.cs
@@ -1,20 +1,38 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 
 namespace BlazorApp.Models.Account
 {
     public class EditUser
     {
         [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
 
         [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
 
-        [Required]
+        //[Required]
+        //public string FirstName { get; set; }
+
+        //[Required]
+        //public string LastName { get; set; }
+
+        [Display(Name = "Username")] // comment out later if needed
         public string Username { get; set; }
 
+        [Required]
+        public string Country { get; set; }
+
+        // [Required]
         [MinLength(6, ErrorMessage = "The Password field must be a minimum of 6 characters")]
         public string Password { get; set; }
+
+        // [Required]
+        // [CompareProperty("Password")]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
 
         public EditUser() { }
 
